394fba8be4e27666b37dcdfd46626dad06c28703
Lebranc Convas <lebrancconvas@gmail.com>
2018-01-20 00:26:55 +0700

    Initial Commit

---
+++ fortune-pookie/src/App.js
@@ -0,0 +1,30 @@
+import React, {
+    Component
+} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return ( <
+            div className = "App" >
+            <
+            header className = "App-header" >
+            <
+            img src = {
+                logo
+            }
+            className = "App-logo"
+            alt = "logo" / >
+            <
+            h1 className = "App-title" > Welcome to Fortune - Pookie < /h1> < /
+            header > <
+            p className = "App-intro" >
+            Fortune Pookie. <
+            /p> < /
+            div >
+        );
+    }
+}
+
+export default App;
